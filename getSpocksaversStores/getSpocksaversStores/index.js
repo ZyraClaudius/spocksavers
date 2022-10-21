6bd95bafd091a51baf730588a3f9a904fa02880d
Zee Alvarez-Claudius <zee.alvarez-claudius@MacBook-Pro.local>
2022-10-21 12:12:25 +0100

    Clean up

--- getSpocksaversStores/getSpocksaversStores/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function (context, req) {
-    const responseMessage = "Hello World!"
-
-    context.res = {
-        status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
